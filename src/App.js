35841a0aa5b760eb869847151cf044bf593b4ed7
v8phan <56612604+v8phan@users.noreply.github.com>
2020-02-17 17:42:02 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+<div>
+  <Navbar />
+  <h1>Hello World!</h1>
+</div>
+  );
+}
+
+export default App;
